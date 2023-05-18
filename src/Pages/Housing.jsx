f93c8c9da5f608e7f7b1020cb448aaf1c37a9f21
yohann <lesage.ly@gmail.com>
2023-05-18 19:57:49 +0200

    update error

--- src/Pages/Housing.jsx
+++ src/Pages/Housing.jsx
@@ -6,62 +6,55 @@ import Star from '../Components/Score'
 import { useParams } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 
-
-
 function Housing() {
-
-    
     const [item, setItem] = useState(null)
     const { id } = useParams()
-    
-    useEffect(() => {  
-      fetch('../Data/kasa-data.json')
-        .then(response => {return response.json()})
-        .then(jsonData => {  
-          let itemAppart = jsonData.find((item) => item.id === id)
-          if (!itemAppart) {
-            return (
-              <Error />
-            )
-          }
-          setItem(itemAppart)     
-          })
-        .catch(error => console.error('Erreur : ', error))    
+
+    useEffect(() => {
+        fetch('../Data/kasa-data.json')
+            .then(response => response.json())
+            .then(jsonData => {
+                let itemAppart = jsonData.find(item => item.id === id)
+                setItem(itemAppart)
+            })
+            .catch(error => console.error('Erreur : ', error))
     }, [id])
 
+    if (!item) {
+        return <Error />
+    }
 
-        return (
-            item &&
-                <main className='base'>
-                    <Carousel item={item}/>
-                    <section className='base_identity'>
-                        <div className='base_identity_item'>
-                            <h1 className='base_identity_item_title_h1'>{item.title}</h1>
-                            <p className='base_identity_item_title_address'>{item.location}</p>
-                            <Category item={item}/>
-                        </div>
-                        <div className='base_identity_profil'>
-                            <div className='base_identity_profil_name'>
-                                <p className='base_identity_profil_name_N'>{item.host.name}</p>
-                            </div>
-                            <div className='base_identity_profil_name_I'>
-                                <img src={item.host.picture} alt='Photographie de l hote' />
-                            </div>
-                            <div className='base_identity_profil_rate'>
-                            <Star item={item}/>
-                            </div>                
-                        </div>
-                    </section>
-                    <section className='base_collapse'>
-                        <div className='base_collapse_item'>
-                        <Collapse label="Description" title="Description" content={item.description} />
-                        </div>
-                        <div className='base_collapse_item'>
-                        <Collapse label="Equiepements" title="Equipements" content={item.equipments}/>
-                        </div>
-                    </section>
-                </main>            
-        )    
+    return (
+        <main className='base'>
+            <Carousel item={item}/>
+            <section className='base_identity'>
+                <div className='base_identity_item'>
+                    <h1 className='base_identity_item_title_h1'>{item.title}</h1>
+                    <p className='base_identity_item_title_address'>{item.location}</p>
+                    <Category item={item}/>
+                </div>
+                <div className='base_identity_profil'>
+                    <div className='base_identity_profil_name'>
+                        <p className='base_identity_profil_name_N'>{item.host.name}</p>
+                    </div>
+                    <div className='base_identity_profil_name_I'>
+                        <img src={item.host.picture} alt='Photographie de l hote' />
+                    </div>
+                    <div className='base_identity_profil_rate'>
+                        <Star item={item}/>
+                    </div>                
+                </div>
+            </section>
+            <section className='base_collapse'>
+                <div className='base_collapse_item'>
+                    <Collapse label="Description" title="Description" content={item.description} />
+                </div>
+                <div className='base_collapse_item'>
+                    <Collapse label="Equiepements" title="Equipements" content={item.equipments}/>
+                </div>
+            </section>
+        </main>            
+    )    
 }
 
 export default Housing
